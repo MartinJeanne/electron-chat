1deafe0f1f00964dae2ba02fdaf7cc2173a32256
MartinJeanne <martinjeanne.dev@gmail.com>
2024-09-11 15:18:33 +0200

    global.d.ts

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+// global.d.ts
+export {};
+
+declare global {
+  interface Window {
+    versions: {
+      node: () => string;
+      chrome: () => string;
+      electron: () => string;
+    };
+  }
+}
