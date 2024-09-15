03a5fc0bf4f82cc209abfd1b6183ba06c9254010
MartinJeanne <martinjeanne.dev@gmail.com>
2024-09-15 19:53:25 +0200

    better status

--- src/renderer/info.ts
+++ src/renderer/info.ts
@@ -2,7 +2,6 @@ const chatAPI = window.chatAPI;
 
 const connectBtn = document.querySelector('.btn.connect') as HTMLButtonElement;
 const disconnectBtn = document.querySelector('.btn.disconnect') as HTMLButtonElement;
-const statusTxt = document.querySelector('#status') as HTMLParagraphElement;
 const namePlaceholder = document.getElementById('namePlaceholder') as HTMLParagraphElement;
 const chatbox = document.querySelector('#chatbox') as HTMLInputElement;
 
@@ -11,23 +10,32 @@ export default function setUpInfo() {
         if (!namePlaceholder) throw new Error('namePlaceholder not found');
 
         console.log('renderer trigered:' + name);
-        namePlaceholder.innerText = name;
+        namePlaceholder.innerText = `Your name is: ${name}`;
     });
 
     connectBtn.addEventListener('click', () => {
         chatAPI.connectToWS();
-        statusTxt.textContent = 'connected';
-        connectBtn.disabled = true;
-        disconnectBtn.disabled = false;
-        chatbox.disabled = false;
+        switchStatus();
     });
 
     disconnectBtn.addEventListener('click', () => {
         chatAPI.disconnectToWS();
-        statusTxt.textContent = 'offline';
-        namePlaceholder.innerText = '';
-        connectBtn.disabled = false;
-        disconnectBtn.disabled = true;
-        chatbox.disabled = true;
+        switchStatus();
     });
+}
+
+function switchStatus() {
+    const connected = disconnectBtn.disabled;
+
+    connectBtn.disabled = connected;
+    disconnectBtn.disabled = !connected;
+    chatbox.disabled = !connected;
+    if (!connected) {
+        connectBtn.textContent = 'Connect';
+        disconnectBtn.textContent = 'Disconnected';
+        namePlaceholder.innerText = '';
+    } else {
+        connectBtn.textContent = 'Connected';
+        disconnectBtn.textContent = 'Disconnect';
+    }
 }
