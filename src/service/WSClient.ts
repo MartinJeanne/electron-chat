8a3e2dc3011570ae91c85bd261d13e0b623cac22
MartinJeanne <martinjeanne.dev@gmail.com>
2025-04-12 18:56:47 +0200

    changing port

--- src/service/WSClient.ts
+++ src/service/WSClient.ts
@@ -7,7 +7,7 @@ export default class WSClient {
     private name: string;
 
     constructor(url?: string) {
-        this.wsURL = url ? url : 'ws://linkstart.club:8080';
+        this.wsURL = url ? url : 'ws://linkstart.club:8082';
         this.ws = null;
         this.name = '';
     }
